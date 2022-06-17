7ba682e31caa91fe76ee27829615da03288f6857
taetae <peacepiece7@naver.com>
2022-06-17 21:52:50 +0900

    add 06/17 rendering csr, ssr, 기술및문제정리, 협업'

--- front_academy_kimmintae/reference/@types_resolve.ts
+++
@@ -1,9 +0,0 @@
-import { v4 } from "uuid";
-
-// uuid package 랜덤 id생성
-type UniqObject = {
-  id: string;
-  [key: string]: string | number | boolean;
-};
-
-const makeObject = (): UniqObject => ({});
